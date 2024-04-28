cfa75aeb403308e2502f69b346555a579dbcf4cc
Adolfo Nava <adolfonava1990@gmail.com>
2024-04-28 19:13:14 +0000

    Finished game

--- app.rb
+++ app.rb
@@ -2,8 +2,61 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:main)
+end
+get("/rock") do
+  options = ['rock', 'paper', 'scissors']
+  @cpu = options.sample
+  @player = 'rock'
+  case @cpu
+  when 'rock'
+    @results = 'tied'
+  when 'paper'
+    @results = 'lost'
+  when 'scissors'
+    @results = 'won'
+  else
+  
+  end
+  
+  erb(:game)
+  
+end
+
+get("/paper") do
+  options = ['rock', 'paper', 'scissors']
+  @cpu = options.sample
+  @player = 'paper'
+  case @cpu
+  when 'rock'
+    @results = 'won'
+  when 'paper'
+    @results = 'tied'
+  when 'scissors'
+    @results = 'lost'
+  else
+  
+  end
+  
+  erb(:game)
+  
+end
+
+get("/scissors") do
+  options = ['rock', 'paper', 'scissors']
+  @cpu = options.sample
+  @player = 'scissors'
+  case @cpu
+  when 'rock'
+    @results = 'lost'
+  when 'paper'
+    @results = 'won'
+  when 'scissors'
+    @results = 'tied'
+  else
+  
+  end
+  
+  erb(:game)
+  
 end
